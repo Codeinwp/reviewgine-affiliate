f802e5738146eb5edceeb433ced0dd49c3d0556e
mariusalex20 <cristea.marius.alexandru@gmail.com>
2014-04-18 18:05:00 +0300

    cleaning

--- admin/functions.php
+++
@@ -1,8 +0,0 @@
-<?php
-		require( get_template_directory() . '/admin/inc/config.php' ); 
-		require( get_template_directory() . '/admin/inc/adminOptionsValidator.php' );
-		require( get_template_directory() . '/admin/inc/adminSanitizer.php' );
-		require( get_template_directory() . '/admin/inc/render.php' ); 
-		require( get_template_directory() . '/admin/inc/loader.php' );  
-		
-?>
